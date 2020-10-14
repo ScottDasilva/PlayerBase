c19fd4fe7d19521c76232ad6f24804830df91139
Scott Da Silva <dasilva_scott@hotmail.com>
2020-10-14 15:30:15 -0400

    added a db initializer to seed roles

---
+++ PlayerBase_3/Models/AppSecrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerBase_3.Models
+{
+    public class AppSecrets
+    {
+        public string ManagerPwd { get; set; }
+        public string PlayerPwd { get; set; }
+    }
+}
--- PlayerBase_3/Program.cs
+++ PlayerBase_3/Program.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PlayerBase_3.Data;
+using PlayerBase_3.Models;
 
 namespace PlayerBase_3
 {
@@ -13,7 +16,17 @@ namespace PlayerBase_3
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            var configuration = host.Services.GetService<IConfiguration>();
+            var hosting = host.Services.GetService<IWebHostEnvironment>();
+
+            var secrets = configuration.GetSection("Secrets").Get<AppSecrets>();
+            DbInitializer.appSecrets = secrets;
+
+            using (var scope = host.Services.CreateScope())
+                DbInitializer.SeedUsersAndRoles(scope.ServiceProvider).Wait();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
