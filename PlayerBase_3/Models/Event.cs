7b41bca2e183011ac0af371b6e0346eee8c92e48
Scott Da Silva <dasilva_scott@hotmail.com>
2020-11-26 08:58:04 -0500

    change env var for deployment

--- PlayerBase_3/Models/Event.cs
+++ PlayerBase_3/Models/Event.cs
@@ -16,11 +16,14 @@ namespace PlayerBase_3.Models
         public int TeamId { get; set; }
         public string Title { get; set; }
         [Required,Display(Name = "Start Time")]
+        [DataType(DataType.Time)]
         public DateTime? StartTime { get; set; }
         [Display(Name = "End Time")]
+        [DataType(DataType.Time)]
         public DateTime? EndTime { get; set; }
         public string Location { get; set; }
         [Required]
+        [DataType(DataType.Date)]
         public DateTime? Date { get; set; }
         public string Description { get; set; }
     }
